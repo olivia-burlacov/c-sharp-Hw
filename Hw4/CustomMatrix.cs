b5bbab63c34f798826aa4def0168d2879eff4328
Olivia Burlacov <olivia.burlacov@gmail.com>
2022-04-27 14:02:11 +0300

    HW 4 methods

---
+++ Hw4/CustomMatrix.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Hw4
+{
+    class CustomMatrix
+    {
+        public static void GetSummOfDiagonalsElements(int[,] matrix)
+        {
+            PrintMatrix(matrix);
+            if (HasMatrixDiagonals(matrix))
+            {
+                int sumD1 = SumFirstDiagonal(matrix);
+                int sumD2 = SumSecondDiagonal(matrix);
+            }
+        }
+
+
+
+
+        public static bool HasMatrixDiagonals(int[,] matrix)
+        {
+
+            int rows = matrix.GetLength(0);
+            int col = matrix.GetLength(1);
+            if (rows != col)
+            {
+                Console.WriteLine("This matrix doesn't have diagonals");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("This matrix has diagonals");
+                return true;
+            }
+        }
+        public static int SumFirstDiagonal(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int col = matrix.GetLength(1);
+            int firstDiagonalSumm = 0;
+            for (var i = 0; i < rows; i++)
+            {
+                for (var j = 0; j < col; j++)
+                {
+                    if (i == j)
+                    {
+                        firstDiagonalSumm = firstDiagonalSumm + matrix[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("First Diagonal Summ is " + firstDiagonalSumm);
+            return firstDiagonalSumm;
+        }
+        public static int SumSecondDiagonal(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int col = matrix.GetLength(1);
+            int secondDiagonalSumm = 0;
+
+            for (var i = 0; i < rows; i++)
+            {
+                for (var j = 0; j < col; j++)
+                {
+                    if ((i + j) == (col - 1))
+                    {
+                        secondDiagonalSumm = secondDiagonalSumm + matrix[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("Second Diagonal Summ is " + secondDiagonalSumm);
+            return secondDiagonalSumm;
+        }
+        public static void PrintMatrix(int[,] matrix)
+        {
+
+
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
+            for (var i=0; i<matrix.GetLength(0); i++)
+            {
+
+                Console.WriteLine();
+                for (var j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i,j] + " ");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Hw4/EntryPoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw4
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[3, 3] {
+                { 1,   3,  5},
+                { 2, 3, 5},
+                {100, 56 , 4} };
+            int[,] matrix2 = new int[3, 4] {
+                { 1,   3,  5,   6},
+                { 2,   3,  5,  78},
+                {100, 56 , -54, 6} };
+            int[,] matrix3 = new int[4, 4] {
+                { 1,   3,  5,   6},
+                { 2,   3,  5,  78},
+                {100, 56 , -54, 6},
+                {10, 6 , -4, 61} };
+
+            CustomMatrix.GetSummOfDiagonalsElements(matrix);
+            CustomMatrix.GetSummOfDiagonalsElements(matrix2);
+            CustomMatrix.GetSummOfDiagonalsElements(matrix3);
+
+        }
+    }
+}
