4d513ccc2672355667dc1e01b40d8e70c2e3150e
Olivia Burlacov <olivia.burlacov@gmail.com>
2022-04-30 20:54:38 +0300

    hw6

---
+++ Hw6/Polymorphism.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw6
+{
+    //Main abstract class
+    abstract class User
+    {
+        protected string name;
+        public abstract void IntroduceSelf();
+       
+    }
+
+    //Derived Student class
+
+    class Student : User
+    {
+        private int age;
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public override void IntroduceSelf()
+        {
+            Console.WriteLine("Hi! My name is " + name + ". I'm " + age + " years old.");
+        }
+
+    }
+
+    //Derived Teacher class
+    class Teacher : User
+    {
+        private string subject;
+        public Teacher(string name, string subject)
+        {
+            this.name = name;
+            this.subject = subject;
+        }
+
+        public override void IntroduceSelf()
+        {
+            Console.WriteLine("Hello! My name is " + name + ". I will be your " + subject + " teacher.");
+        }
+
+    }
+}
---
+++ Hw6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hw6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("John", 20);
+            Student s2 = new Student("Ann", 20);
+            Teacher t1 = new Teacher("Malcolm", "Mathematics");
+            Teacher t2 = new Teacher("Jane", "Literature");
+
+            User[] users = { s1, s2, t1, t2 };
+            
+            foreach (User item in users)
+            {
+                item.IntroduceSelf();
+            }
+        }
+    }
+}
